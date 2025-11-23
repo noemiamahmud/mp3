272fc43eae310f1d06041ea66d5c2bcacd8e9921
noemiamahmud <149017399+noemiamahmud@users.noreply.github.com>
2025-11-23 17:59:00 -0600

    finalized code

--- database_scripts/dbClean.py
+++ database_scripts/dbClean.py
@@ -1,105 +1,75 @@
 #!/usr/bin/env python
 
 """
- * @file dbClean.py
- * Used in CS498RK MP4 to empty database of all users and tasks.
- *
- * @author Aswin Sivaraman
- * @date Created: Spring 2015
- * @date Modified: Spring 2015
- * @date Modified: Spring 2019
+ * dbClean.py
+ * Empties DB of all users and tasks (CS498 RK MP3 / MP4 helper)
 """
 
 import sys
 import getopt
 import http.client
-import urllib
 import json
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 def usage():
     print('dbClean.py -u <baseurl> -p <port>')
 
 def getUsers(conn):
-    # Retrieve the list of users
-    conn.request("GET","""/api/users?filter={"_id":1}""")
+    conn.request("GET", """/api/users?filter={"_id":1}""")
     response = conn.getresponse()
     data = response.read()
     d = json.loads(data)
-
-    # Array of user IDs
-    users = [str(d['data'][x]['_id']) for x in range(len(d['data']))]
-
-    return users
+    return [str(item["_id"]) for item in d.get("data", [])]
 
 def getTasks(conn):
-    # Retrieve the list of tasks
-    conn.request("GET","""/api/tasks?filter={"_id":1}""")
+    conn.request("GET", """/api/tasks?filter={"_id":1}""")
     response = conn.getresponse()
     data = response.read()
     d = json.loads(data)
-
-    # Array of user IDs
-    tasks = [str(d['data'][x]['_id']) for x in range(len(d['data']))]
-
-    return tasks
+    return [str(item["_id"]) for item in d.get("data", [])]
 
 def main(argv):
-
-    # Server Base URL and port
     baseurl = "localhost"
     port = 4000
 
     try:
-        opts, args = getopt.getopt(argv,"hu:p:",["url=","port="])
+        opts, args = getopt.getopt(argv, "hu:p:", ["url=", "port="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
+
     for opt, arg in opts:
-        if opt == '-h':
-             usage()
-             sys.exit()
+        if opt == "-h":
+            usage()
+            sys.exit()
         elif opt in ("-u", "--url"):
-             baseurl = str(arg)
+            baseurl = str(arg)
         elif opt in ("-p", "--port"):
-             port = int(arg)
+            port = int(arg)
 
-    # Server to connect to (1: url, 2: port number)
-    conn = http.client.HTTPConnection(baseurl, port)
+    #changed to HTTPS for Render
+    conn = http.client.HTTPSConnection(baseurl, port)
 
-    # Fetch a list of users
+    # --- DELETE USERS ---
     users = getUsers(conn)
-
-    # Loop for as long as the database still returns users
     while len(users):
-
-        # Delete each individual user
         for user in users:
-            conn.request("DELETE","/api/users/"+user)
-            response = conn.getresponse()
-            data = response.read()
-
-        # Fetch a list of users
+            conn.request("DELETE", "/api/users/" + user)
+            conn.getresponse().read()
         users = getUsers(conn)
 
-    # Fetch a list of tasks
+    # --- DELETE TASKS ---
     tasks = getTasks(conn)
-
-    # Loop for as long as the database still returns tasks
     while len(tasks):
-
-        # Delete each individual task
         for task in tasks:
-            conn.request("DELETE","/api/tasks/"+task)
-            response = conn.getresponse()
-            data = response.read()
-
-        # Fetch a list of tasks
+            conn.request("DELETE", "/api/tasks/" + task)
+            conn.getresponse().read()
         tasks = getTasks(conn)
 
-    # Exit gracefully
     conn.close()
-    print("All users and tasks removed at "+baseurl+":"+str(port))
-
+    print(f"All users and tasks removed at {baseurl}:{port}")
 
 if __name__ == "__main__":
-     main(sys.argv[1:])
+    main(sys.argv[1:])
--- database_scripts/dbFill.py
+++ database_scripts/dbFill.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 
 """
- * @file dbFill.py
- * Used in CS498RK MP4 to populate database with randomly generated users and tasks.
- *
- * @author Aswin Sivaraman
- * @date Created: Spring 2015
- * @date Modified: Spring 2015
- * @date Modified: Spring 2019
+ * FIXED dbFill.py
+ * Fully compatible with Render + your API
+ * Includes HTTPS, SSL bypass, retry logic, safe parsing
 """
 
 import sys
@@ -15,143 +11,158 @@ import getopt
 import http.client
 import urllib
 import json
-from random import randint
-from random import choice
+from random import randint, choice
 from datetime import date
 from time import mktime
+import ssl
+import time
+
+# Disable SSL verification (Render cert chain mismatch on macOS Python)
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 def usage():
     print('dbFill.py -u <baseurl> -p <port> -n <numUsers> -t <numTasks>')
 
-def getUsers(conn):
-    # Retrieve the list of users
-    conn.request("GET","""/api/users?filter={"_id":1}""")
-    response = conn.getresponse()
-    data = response.read()
-    d = json.loads(data)
 
-    # Array of user IDs
-    users = [str(d['data'][x]['_id']) for x in range(len(d['data']))]
+def safe_json(raw):
+    """Safely parse JSON or return empty dict."""
+    try:
+        return json.loads(raw)
+    except:
+        return {}
 
-    return users
+
+def post_with_retry(conn, path, params, headers):
+    """
+    Makes POST request with automatic retry for rate limits (429).
+    Returns parsed JSON.
+    """
+
+    for attempt in range(5):  # retry up to 5 times
+        conn.request("POST", path, params, headers)
+        response = conn.getresponse()
+        raw = response.read()
+        data = safe_json(raw)
+
+        status = response.status
+
+        if status == 429:
+            print("⚠️  Hit 429 rate limit — retrying...")
+            time.sleep(1.0)
+            continue
+
+        if status >= 400:
+            print(f"❌ POST {path} failed ({status}):", raw)
+            return None
+
+        return data
+
+    print(f"❌ POST {path} failed too many times.")
+    return None
+
 
 def main(argv):
-
-    # Server Base URL and port
     baseurl = "localhost"
     port = 4000
-
-    # Number of POSTs that will be made to the server
     userCount = 50
     taskCount = 200
 
+    # Parse args
     try:
-        opts, args = getopt.getopt(argv,"hu:p:n:t:",["url=","port=","users=","tasks="])
+        opts, args = getopt.getopt(argv, "hu:p:n:t:", ["url=", "port=", "users=", "tasks="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
+
     for opt, arg in opts:
-        if opt == '-h':
-             usage()
-             sys.exit()
+        if opt == "-h":
+            usage()
+            sys.exit()
         elif opt in ("-u", "--url"):
-             baseurl = str(arg)
+            baseurl = str(arg)
         elif opt in ("-p", "--port"):
-             port = int(arg)
+            port = int(arg)
         elif opt in ("-n", "--users"):
-             userCount = int(arg)
+            userCount = int(arg)
         elif opt in ("-t", "--tasks"):
-             taskCount = int(arg)
+            taskCount = int(arg)
 
-    # Python array containing common first names and last names
-    firstNames = ["james","john","robert","michael","william","david","richard","charles","joseph","thomas","christopher","daniel","paul","mark","donald","george","kenneth","steven","edward","brian","ronald","anthony","kevin","jason","matthew","gary","timothy","jose","larry","jeffrey","frank","scott","eric","stephen","andrew","raymond","gregory","joshua","jerry","dennis","walter","patrick","peter","harold","douglas","henry","carl","arthur","ryan","roger","joe","juan","jack","albert","jonathan","justin","terry","gerald","keith","samuel","willie","ralph","lawrence","nicholas","roy","benjamin","bruce","brandon","adam","harry","fred","wayne","billy","steve","louis","jeremy","aaron","randy","howard","eugene","carlos","russell","bobby","victor","martin","ernest","phillip","todd","jesse","craig","alan","shawn","clarence","sean","philip","chris","johnny","earl","jimmy","antonio","danny","bryan","tony","luis","mike","stanley","leonard","nathan","dale","manuel","rodney","curtis","norman","allen","marvin","vincent","glenn","jeffery","travis","jeff","chad","jacob","lee","melvin","alfred","kyle","francis","bradley","jesus","herbert","frederick","ray","joel","edwin","don","eddie","ricky","troy","randall","barry","alexander","bernard","mario","leroy","francisco","marcus","micheal","theodore","clifford","miguel","oscar","jay","jim","tom","calvin","alex","jon","ronnie","bill","lloyd","tommy","leon","derek","warren","darrell","jerome","floyd","leo","alvin","tim","wesley","gordon","dean","greg","jorge","dustin","pedro","derrick","dan","lewis","zachary","corey","herman","maurice","vernon","roberto","clyde","glen","hector","shane","ricardo","sam","rick","lester","brent","ramon","charlie","tyler","gilbert","gene"]
-    lastNames = ["smith","johnson","williams","jones","brown","davis","miller","wilson","moore","taylor","anderson","thomas","jackson","white","harris","martin","thompson","garcia","martinez","robinson","clark","rodriguez","lewis","lee","walker","hall","allen","young","hernandez","king","wright","lopez","hill","scott","green","adams","baker","gonzalez","nelson","carter","mitchell","perez","roberts","turner","phillips","campbell","parker","evans","edwards","collins","stewart","sanchez","morris","rogers","reed","cook","morgan","bell","murphy","bailey","rivera","cooper","richardson","cox","howard","ward","torres","peterson","gray","ramirez","james","watson","brooks","kelly","sanders","price","bennett","wood","barnes","ross","henderson","coleman","jenkins","perry","powell","long","patterson","hughes","flores","washington","butler","simmons","foster","gonzales","bryant","alexander","russell","griffin","diaz","hayes"]
+    # Sample names
+    firstNames = ["james", "john", "robert", "michael", "william",
+                  "david", "richard", "charles", "joseph", "thomas"]
+    lastNames = ["smith", "johnson", "williams", "jones", "brown",
+                 "davis", "miller", "wilson", "moore", "taylor"]
 
-    # Server to connect to (1: url, 2: port number)
-    conn = http.client.HTTPConnection(baseurl, port)
+    conn = http.client.HTTPSConnection(baseurl, port)
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "application/json"}
 
-    # HTTP Headers
-    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-
-    # Array of user IDs
     userIDs = []
     userNames = []
-    userEmails = []
 
-    # Loop 'userCount' number of times
+    print("➡️ Creating users...")
+
+    # --------------------------------------------------------------
+    # CREATE USERS
+    # --------------------------------------------------------------
     for i in range(userCount):
+        x = randint(0, len(firstNames)-1)
+        y = randint(0, len(lastNames)-1)
 
-        # Pick a random first name and last name
-        x = randint(0,99)
-        y = randint(0,99)
-        params = urllib.parse.urlencode({'name': firstNames[x] + " " + lastNames[y], 'email': firstNames[x] + "@" + lastNames[y] + ".com"})
+        params = urllib.parse.urlencode({
+            'name': firstNames[x] + " " + lastNames[y],
+            'email': f"{firstNames[x]}_{randint(1,99999)}@{lastNames[y]}.com"  # avoid duplicates
+        })
 
-        # POST the user
-        conn.request("POST", "/api/users", params, headers)
-        response = conn.getresponse()
-        data = response.read()
-        d = json.loads(data)
+        result = post_with_retry(conn, "/api/users", params, headers)
+        if not result or "data" not in result or "_id" not in result["data"]:
+            print("❌ Skipping user due to bad response:", result)
+            continue
 
-        # Store the users id
-        userIDs.append(str(d['data']['_id']))
-        userNames.append(str(d['data']['name']))
-        userEmails.append(str(d['data']['email']))
+        userIDs.append(result["data"]["_id"])
+        userNames.append(result["data"]["name"])
 
-    # Open 'tasks.txt' for sample task names
-    f = open('tasks.txt','r')
-    taskNames = f.read().splitlines()
+    print(f"✅ Created {len(userIDs)} users")
 
-    # Loop 'taskCount' number of times
+    # --------------------------------------------------------------
+    # CREATE TASKS
+    # --------------------------------------------------------------
+    print("➡️ Creating tasks...")
+
+    with open("tasks.txt", "r") as f:
+        taskNames = f.read().splitlines()
+
     for i in range(taskCount):
 
-        # Randomly generate task parameters
-        assigned = (randint(0,10) > 4)
-        assignedUser = randint(0,len(userIDs)-1) if assigned else -1
-        assignedUserID = userIDs[assignedUser] if assigned else ''
-        assignedUserName = userNames[assignedUser] if assigned else 'unassigned'
-        assignedUserEmail = userEmails[assignedUser] if assigned else 'unassigned'
-        completed = (randint(0,10) > 5)
-        deadline = (mktime(date.today().timetuple()) + randint(86400,864000)) * 1000
-        description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
-        params = urllib.parse.urlencode({'name': choice(taskNames), 'deadline': deadline, 'assignedUserName': assignedUserName, 'assignedUser': assignedUserID, 'completed': str(completed).lower(), 'description': description})
+        assigned = (randint(0, 10) > 4)
+        completed = (randint(0, 10) > 5)
 
-        # POST the task
-        conn.request("POST", "/api/tasks", params, headers)
-        response = conn.getresponse()
-        data = response.read()
-        d = json.loads(data)
+        if assigned and userIDs:
+            idx = randint(0, len(userIDs)-1)
+            assignedUserID = userIDs[idx]
+            assignedUserName = userNames[idx]
+        else:
+            assignedUserID = ""
+            assignedUserName = "unassigned"
 
-        taskID = str(d['data']['_id'])
+        deadline = (mktime(date.today().timetuple()) + randint(86400, 864000)) * 1000
 
-        # Make sure the task is added to the pending list of the user
-        if assigned and not completed:
-            # GET the correct user
-            conn.request("GET","""/api/users?where={"_id":\""""+assignedUserID+"""\"}""")
-            response = conn.getresponse()
-            data = response.read()
-            d = json.loads(data)
+        params = urllib.parse.urlencode({
+            'name': choice(taskNames),
+            'deadline': deadline,
+            'description': "Autogenerated task.",
+            'assignedUser': assignedUserID,
+            'assignedUserName': assignedUserName,
+            'completed': str(completed).lower()
+        })
 
-            # Store all the user properties
-            assignedUserName = str(d['data'][0]['name'])
-            assignedUserEmail = str(d['data'][0]['email'])
-            assignedUserDate = str(d['data'][0]['dateCreated'])
+        result = post_with_retry(conn, "/api/tasks", params, headers)
+        if not result:
+            print("❌ Task failed:", result)
 
-            # Append the new taskID to pending tasks
-            assignedUserTasks = d['data'][0]['pendingTasks']
-            assignedUserTasks = [str(x).replace('[','').replace(']','').replace("'",'').replace('"','') for x in assignedUserTasks]
-            assignedUserTasks.append(taskID)
-
-            # PUT in the user
-            params = urllib.parse.urlencode({'_id': assignedUserID, 'name': assignedUserName, 'email': assignedUserEmail, 'dateCreated': assignedUserDate, 'pendingTasks': assignedUserTasks}, True)
-            conn.request("PUT", "/api/users/"+assignedUserID, params, headers)
-            response = conn.getresponse()
-            data = response.read()
-            d = json.loads(data)
-
-    # Exit gracefully
     conn.close()
-    print(str(userCount)+" users and "+str(taskCount)+" tasks added at "+baseurl+":"+str(port))
+    print(f"🎉 DONE — Added {len(userIDs)} users and {taskCount} tasks at {baseurl}:{port}")
 
 
 if __name__ == "__main__":
-     main(sys.argv[1:])
+    main(sys.argv[1:])
